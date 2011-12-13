f9d163c64f530eaddd2cae7a4771610c07749c73
Craig Hawker <Craig Hawker@localhost>
2011-12-13 20:52:25 +0000

    Added a UniqueValidator (to be used to confirm uniqueness of identifier elements) Added 3 tests for UniqueValidator (2 valid, 1 invalid)

---
+++ src/TestProject/XmlValidationTests/ContentValidation/UniqueValidator.Valid.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.XmlValidationTests.ContentValidation
+{
+    public partial class UniqueValidator
+    {
+        [TestMethod]
+        [DeploymentItem
+            (
+            @"TestProject\Resources\ContentValidation\UniqueValidator\Valid\UniqueIdentifiersWithXsiType.xml",
+            @"ContentValidation\UniqueValidator\Valid\"
+            )]
+        public void Valid_UniqueIdentifiersWithXsiType()
+        {
+            var xDoc = System.Xml.Linq.XDocument.Load
+                (
+                new System.IO.FileInfo(@"ContentValidation\UniqueValidator\Valid\UniqueIdentifiersWithXsiType.xml").FullName,
+                System.Xml.Linq.LoadOptions.SetLineInfo
+                );
+            System.Xml.XmlNamespaceManager nsmgr = new System.Xml.XmlNamespaceManager(new System.Xml.NameTable());
+            nsmgr.AddNamespace("dc", "http://purl.org/dc/elements/1.1/");
+            nsmgr.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            XCRI.Validation.ContentValidation.UniqueValidator v = new XCRI.Validation.ContentValidation.UniqueValidator
+            (
+                null,
+                nsmgr,
+                "//dc:identifier[not(@xsi:type)]",
+                "All identifier elements without xsi:type attributes must be unique",
+                XCRI.Validation.ContentValidation.ValidationStatus.Exception
+            );
+            var r = v.Validate(xDoc.Root);
+            Assert.IsNotNull(r);
+            Assert.AreEqual<XCRI.Validation.ContentValidation.ValidationStatus>
+                (
+                XCRI.Validation.ContentValidation.ValidationStatus.Valid,
+                r.Status
+                );
+            Assert.AreEqual<int>
+                (
+                2,
+                r.Count
+                );
+            Assert.AreEqual<int>
+                (
+                2,
+                r.SuccessCount
+                );
+            Assert.AreEqual<int>
+                (
+                0,
+                r.FailedCount
+                );
+            Assert.IsTrue(r.Instances[0].LineNumber.HasValue);
+            Assert.IsTrue(r.Instances[0].LinePosition.HasValue);
+            Assert.AreEqual<int>(r.Instances[0].LineNumber.Value, 11);
+            Assert.AreEqual<int>(r.Instances[0].LinePosition.Value, 8);
+            Assert.IsTrue(r.Instances[1].LineNumber.HasValue);
+            Assert.IsTrue(r.Instances[1].LinePosition.HasValue);
+            Assert.AreEqual<int>(r.Instances[1].LineNumber.Value, 15);
+            Assert.AreEqual<int>(r.Instances[1].LinePosition.Value, 8);
+        }
+        [TestMethod]
+        [DeploymentItem
+            (
+            @"TestProject\Resources\ContentValidation\UniqueValidator\Valid\UniqueIdentifiers.xml",
+            @"ContentValidation\UniqueValidator\Valid\"
+            )]
+        public void Valid_UniqueIdentifiers()
+        {
+            var xDoc = System.Xml.Linq.XDocument.Load
+                (
+                new System.IO.FileInfo(@"ContentValidation\UniqueValidator\Valid\UniqueIdentifiers.xml").FullName,
+                System.Xml.Linq.LoadOptions.SetLineInfo
+                );
+            System.Xml.XmlNamespaceManager nsmgr = new System.Xml.XmlNamespaceManager(new System.Xml.NameTable());
+            nsmgr.AddNamespace("dc", "http://purl.org/dc/elements/1.1/");
+            nsmgr.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            XCRI.Validation.ContentValidation.UniqueValidator v = new XCRI.Validation.ContentValidation.UniqueValidator
+            (
+                null,
+                nsmgr,
+                "//dc:identifier[not(@xsi:type)]",
+                "All identifier elements without xsi:type attributes must be unique",
+                XCRI.Validation.ContentValidation.ValidationStatus.Exception
+            );
+            var r = v.Validate(xDoc.Root);
+            Assert.IsNotNull(r);
+            Assert.AreEqual<XCRI.Validation.ContentValidation.ValidationStatus>
+                (
+                XCRI.Validation.ContentValidation.ValidationStatus.Valid,
+                r.Status
+                );
+            Assert.AreEqual<int>
+                (
+                2,
+                r.Count
+                );
+            Assert.AreEqual<int>
+                (
+                2,
+                r.SuccessCount
+                );
+            Assert.AreEqual<int>
+                (
+                0,
+                r.FailedCount
+                );
+            Assert.IsTrue(r.Instances[0].LineNumber.HasValue);
+            Assert.IsTrue(r.Instances[0].LinePosition.HasValue);
+            Assert.AreEqual<int>(r.Instances[0].LineNumber.Value, 11);
+            Assert.AreEqual<int>(r.Instances[0].LinePosition.Value, 6);
+            Assert.IsTrue(r.Instances[1].LineNumber.HasValue);
+            Assert.IsTrue(r.Instances[1].LinePosition.HasValue);
+            Assert.AreEqual<int>(r.Instances[1].LineNumber.Value, 18);
+            Assert.AreEqual<int>(r.Instances[1].LinePosition.Value, 8);
+        }
+    }
+}
---
+++ src/XCRI.Validation/ContentValidation/UniqueValidator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XCRI.Validation.ContentValidation
+{
+    public class UniqueValidator : Validator
+    {
+        public UnqiueAcrossTypes UniqueAcross { get; set; }
+        public UniqueValidator
+            (
+            IEnumerable<MessageInterpretation.IInterpreter> interpreters,
+            XmlNamespaceManager namespaceManager,
+            string xPathSelector,
+            string exceptionMessage,
+            ValidationStatus failedValidationStatus
+            )
+            : base(interpreters, namespaceManager, xPathSelector, exceptionMessage, failedValidationStatus)
+        {
+            this.UniqueAcross = UnqiueAcrossTypes.Document;
+        }
+        protected override bool PassesValidation(System.Xml.Linq.XElement input, out string details)
+        {
+            switch (this.UniqueAcross)
+            {
+                case UnqiueAcrossTypes.Document:
+                    details = null;
+                    var value = input.Value;
+                    var sameIdentifiers = input.Document.XPathSelectElements(this.XPathSelector, this.NamespaceManager)
+                        .Where(xe => xe.Value == value);
+                    var count = sameIdentifiers.Count();
+                    if (count == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        details = String.Format
+                            (
+                            "There are {0} other elements with the same identifier as {1} (on line{2} ",
+                            count,
+                            value,
+                            count == 2 ? String.Empty : "s"
+                            );
+                        bool firstItem =true ;
+                        foreach (System.Xml.Linq.XElement xe in sameIdentifiers)
+                        {
+                            if (xe == input)
+                                continue;
+                            if (false == (xe is IXmlLineInfo))
+                                continue;
+                            if (firstItem == false)
+                            {
+                                details += ", ";
+                            }
+                            else
+                            {
+                                firstItem = false;
+                            };
+                            details += (xe as IXmlLineInfo).LineNumber.ToString();
+                        }
+                        details += ")";
+                        return false;
+                    }
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        public enum UnqiueAcrossTypes
+        {
+            Document = 1
+        }
+    }
+}
