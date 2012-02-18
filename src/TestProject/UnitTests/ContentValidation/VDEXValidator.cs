f95b84b4215fbf1e02c71145360dedbbd1281f33
Craig Hawker <Craig Hawker@localhost>
2012-02-18 14:13:06 +0000

    Added VDEXValidator stub unit tests

---
+++ src/TestProject/UnitTests/ContentValidation/VDEXValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XCRI.Validation.XmlRetrieval;
+
+namespace TestProject.UnitTests.ContentValidation
+{
+    [TestClass]
+    public partial class VDEXValidator : IValidator<XCRI.Validation.ContentValidation.VDEXValidator>
+    {
+        public override XCRI.Validation.ContentValidation.VDEXValidator CreateValidator()
+        {
+            throw new NotImplementedException();
+            return new XCRI.Validation.ContentValidation.VDEXValidator(new UriSource(null, null));
+        }
+    }
+}
