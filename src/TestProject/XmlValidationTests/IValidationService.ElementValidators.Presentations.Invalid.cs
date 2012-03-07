569116a1a0211cef45c54dc642e9e6cb6a5c7886
Craig Hawker <Craig Hawker@localhost>
2012-03-07 11:40:37 +0000

    There should be no more than 1 end element in a presentation

--- src/TestProject/XmlValidationTests/IValidationService.ElementValidators.Presentations.Invalid.cs
+++ src/TestProject/XmlValidationTests/IValidationService.ElementValidators.Presentations.Invalid.cs
@@ -67,6 +67,33 @@ namespace TestProject.XmlValidationTests
         }
 
         [TestMethod]
+        public void Invalid_Presentation_WithMultipleEnds()
+        {
+            var elementValidator = this.GetElementValidator_Presentation();
+            elementValidator.Validators.Add(new XCRI.Validation.ContentValidation.NumberValidator()
+            {
+                XPathSelector = "count(./xcri12:end)",
+                ExceptionMessage = "Presentations can contain a maximum of one end element in the XCRI-CAP 1.2 namespace",
+                FailedValidationStatus = XCRI.Validation.ContentValidation.ValidationStatus.Exception,
+                Maximum = 1,
+                ValidationGroup = "Structure",
+                NamespaceManager = elementValidator.NamespaceManager
+            });
+            var vr = elementValidator
+                .Validate(System.Xml.Linq.XDocument.Parse(Resources.IValidationService.Invalid.ElementValidation.Presentations.WithMultipleEnds).Root)
+                .Where(r => r.Message == elementValidator.Validators[0].ExceptionMessage);
+            Assert.AreEqual<int>(1, vr.Count());
+            ValidateResults
+                (
+                result: vr.ElementAt(0),
+                expectedStatus: XCRI.Validation.ContentValidation.ValidationStatus.Exception,
+                expectedInstances: 1,
+                expectedFailedCount: 1,
+                expectedSuccessfulCount: 0
+                );
+        }
+
+        [TestMethod]
         public void Invalid_Presentation_WithoutApplyTo()
         {
             var ev = this.GetElementValidator_Presentation();
