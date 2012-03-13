e35855e79bd7d2a8a35494d900f8afb31f966ae3
Craig Hawker <Craig Hawker@localhost>
2012-03-13 20:40:09 +0000

    Modified subject-must-contain-only-one-keyword rule to be a Warning Modified subject-must-contain-only-one-keyword rule to only select subjects without an xsi:type Modified subject-must-contain-only-one-keyword rule to be a regularexpressionvalidator, allowing ^[A-Za-Z0-9\s]*?$.

---
+++ src/TestProject/UnitTests/ContentValidation/RegularExpressionValidator.Common.Subjects.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.UnitTests.ContentValidation
+{
+    public partial class RegularExpressionValidator
+    {
+        [TestMethod]
+        public void Valid_SubjectOnlyOneKeywordOrPhrase_SingleWord()
+        {
+            Assert.IsTrue(this.PassesValidationString
+                (
+                "hello",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Valid_SubjectOnlyOneKeywordOrPhrase_SingleWordWithNumbers()
+        {
+            Assert.IsTrue(this.PassesValidationString
+                (
+                "H600",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Valid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithSpace()
+        {
+            Assert.IsTrue(this.PassesValidationString
+                (
+                "hello world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithCommaSpace()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello, world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithComma()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello,world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithSemiColonSpace()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello; world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithSemiColon()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello;world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithPipeSpace()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello| world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+        [TestMethod]
+        public void Invalid_SubjectOnlyOneKeywordOrPhrase_TwoWordsWithPipe()
+        {
+            Assert.IsFalse(this.PassesValidationString
+                (
+                "hello|world",
+                Resources.ContentValidation.RegularExpressionValidator.Common.SubjectOnlyOneKeywordOrPhrase
+                ));
+        }
+    }
+}
